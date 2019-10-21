07b84b15ca92aaed7e09cfa588a11bbb910fc4f2
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-21 13:21:25 +0100

    added test for checking balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe do
-    
+require 'oystercard'
+
+describe Oystercard do
+    it 'creates istance of Oystercard' do 
+        expect(subject).to be_an_instance_of(Oystercard)
+    end
+
+    it 'has a starting balance of 0' do
+        expect(subject.balance).to be(0)
+    end
 end
