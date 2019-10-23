91fb69d3a656338d390eed2660b42599027fe551
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-10-23 08:53:58 +0100

    Added station class and tests

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,6 +1,16 @@
-require 'station.rb'
+require 'station'
 
 describe Station do
+    subject {Station.new("Kings Cross", 1)}
+
+    it 'returns the name of the station' do
+        expect(subject.name).to eq("Kings Cross")
+    end
+
+    it 'returns the zone of the station' do
+        expect(subject.zone).to eq(1)
+    end
+
     it 'creates an instance of the station class' do
         expect(subject).to be_an_instance_of(Station)
     end
